3a7531250f9582977af43ce36fcc79784164900f
Marek Balaz <marekgamingacc@gmail.com>
2022-08-22 14:14:21 +0200

    Newtonsoft vulnerability fix

---
+++ Controllers/AuthorizationFilterAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FindProgrammingProject.Controllers
+{
+    public class AuthorizationFilterAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            
+        }
+    }
+}
