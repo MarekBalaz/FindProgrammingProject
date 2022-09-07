63b52c078ed8d92980903bc758d209d4665455c3
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-07 13:56:35 -0700

    refacor: Clean up extra files and add prettier config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Signup from './pages/Signup'
-import ForgotPassword from './pages/ForgotPassword'
-import PasswordReset from './pages/PasswordReset'
-import Login from './pages/Login'
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import PasswordReset from './pages/PasswordReset';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" exact element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
